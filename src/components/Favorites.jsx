80a4392dbcbb9d130d93fccd455e1763d0799e6a
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2025-12-03 16:11:07 +0100

    adds

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,5 +1,6 @@
 import { Row, Col, Button } from "react-bootstrap"
 import { useSelector, useDispatch } from "react-redux"
+import { Link } from "react-router-dom"
 
 const Favorites = () => {
   const favorites = useSelector((state) => {
@@ -9,24 +10,39 @@ const Favorites = () => {
 
   return (
     <Row>
-      <Col sm={10}>
-        <ul className="list-group">
+      <Col sm={12}>
+        <ul className="list-group mx-3">
           {favorites.map((job, index) => (
             <>
-              <li
-                className="my-4 d-flex justify-content-between"
-                key={index.id}
-              >
-                <p>{job.title}</p>
-                <Button
-                  variant="danger"
-                  onClick={() =>
-                    dispatch({ type: "REMMOVE_FAVORITE", payload: job })
-                  }
+              <Row>
+                <li
+                  className="my-4 d-flex justify-content-between"
+                  key={index.id}
                 >
-                  Remove Favorite
-                </Button>
-              </li>
+                  <Col>
+                    <Link to={`/${job.company_name}`}>{job.company_name}</Link>
+                  </Col>
+                  <Col>
+                    <a href={job.url} target="_blank" rel="noreferrer">
+                      {job.title}
+                    </a>
+                  </Col>
+                  <Col>
+                    <span className="mb-0">{job.title}</span>
+                  </Col>
+                  <Col>
+                    <Button
+                      className="ms-3"
+                      variant="danger"
+                      onClick={() =>
+                        dispatch({ type: "REMMOVE_FAVORITE", payload: job })
+                      }
+                    >
+                      Remove Favorite
+                    </Button>
+                  </Col>
+                </li>
+              </Row>
             </>
           ))}
         </ul>
